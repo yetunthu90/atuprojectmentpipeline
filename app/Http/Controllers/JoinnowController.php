a5f3c9653359e00b4d44a6e8ca4e22ea7dee467b
subhashreepan <L00187418@atu.ie>
2025-01-27 23:10:06 +0000

    Code(Studio_Ghibli_Movie_Maker):#BX2024-51-Customer-Module-Display-Customer-Details-and-Management-Features.

--- app/Http/Controllers/JoinnowController.php
+++ app/Http/Controllers/JoinnowController.php
@@ -5,9 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Course;
 use App\Models\Payment;
+use App\Models\CustomerRequest;
 
 class JoinnowController extends Controller
 {
+    public function enquiry_list()
+    {
+       // Fetch all courses
+        $customerrequest = CustomerRequest::all();
+
+        // Pass data to the index view
+        return view('enquiry.index', compact('customerrequest'));
+        // return view('courses.index');
+    }
+
     public function index()
     {
         $courses = Course::all();
